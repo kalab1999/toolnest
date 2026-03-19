90d824656b2e37033625dc2d9530b9de0d75757d
kalu <kalababebe22@gmail.com>
2026-03-19 15:11:47 +0300

    2 ownership verifying

--- app/all-tools/layout.tsx
+++ app/all-tools/layout.tsx
@@ -1,4 +1,15 @@
-<head>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2422025830935555"
-        crossorigin="anonymous"></script>
-</head>
+// app/layout.tsx
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <head>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2422025830935555"
+                    crossOrigin="anonymous"
+                ></script>
+            </head>
+            <body>{children}</body>
+        </html>
+    );
+}
